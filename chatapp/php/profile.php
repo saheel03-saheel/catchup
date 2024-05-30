0dd65697c1407638290acb2b2d140b8c7d692291
Saheel <117504605+saheel03-saheel@users.noreply.github.com>
2024-05-30 16:03:12 +0530

    Add files via upload

---
+++ chatapp/php/profile.php
@@ -0,0 +1,54 @@
+<?php
+  session_start();
+  include_once "..//php//config.php";
+  if(!isset($_SESSION['unique_id'])){
+    header("location: ..//login.php");
+  }
+?>
+
+<?php include_once "..//header.php"; ?>
+
+
+<html>
+
+<link rel="stylesheet" href="..//css//styles2.css">
+
+
+
+<section class="profile">
+  <header class="header">
+    <div class="details">
+
+      <?php
+        $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+        if(mysqli_num_rows($sql) > 0){
+          $row = mysqli_fetch_assoc($sql);
+        }
+      ?>
+
+      <img src="..//php//images/<?php echo $row['img']; ?>" alt="" class="profile-pic">
+
+      <h1 class="heading">
+        <span><?php echo $row['fname']. " " . $row['lname'] ?></span>
+      </h1>
+
+      <div class="location">
+        
+      </div>
+        <div class="col-4">
+          <h4><span><?php echo $row['email']?></span></h4>
+        </div>
+        <div class="col-4">
+          <h4>Hi, I am <?php echo $row['fname']. " " . $row['lname'] ?>. I have a passion for programming and design is my forte. Please feel free to contact me. Thank you.</h4>
+
+        </div>
+      </div>
+
+
+    </div>
+
+    
+  </header>
+</section>
+
+</html>
